c3092c90d0db767dc57ac913aedd245706b86924
JorgeAMartinezV <161075853+JorgeAMartinezV@users.noreply.github.com>
2026-03-05 05:38:26 -0500

    Login created
    Login screen was created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+    implementation("androidx.navigation:navigation-compose:2.8.9")
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
@@ -50,6 +51,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.compose.material.icons.extended)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/canchapp_kotlin/MainActivity.kt
+++ app/src/main/java/com/canchapp_kotlin/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.canchapp_kotlin.ui.theme.CanchappkotlinTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +19,43 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CanchappkotlinTheme {
-                RegisterScreen(
-                    onRegisterWithEmail = { /* navegar a pantalla de form */ },
-                    onLoginClick = { /* navegar a Login */ }
-                )
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = "register"   // Pantalla inicial
+                ) {
+
+                    composable("register") {
+                        RegisterScreen(
+                            onRegisterWithGoogle    = { /* TODO */ },
+                            onRegisterWithInstagram = { /* TODO */ },
+                            onRegisterWithTikTok    = { /* TODO */ },
+                            onRegisterWithEmail     = { /* TODO */ },
+                            onLoginClick = { navController.navigate("login") }
+                        )
+                    }
+
+                    composable("login") {
+                        LoginScreen(
+                            onLoginWithGoogle    = { /* TODO */ },
+                            onLoginWithInstagram = { /* TODO */ },
+                            onLoginWithTikTok    = { /* TODO */ },
+                            onLoginWithEmail = { navController.navigate("login_email") },
+                            onRegisterClick  = { navController.navigate("register") }
+                        )
+                    }
+
+                    composable("login_email") {
+                        LoginEmailScreen(
+                            onBack           = { navController.popBackStack() },
+                            onForgotPassword = { /* TODO */ },
+                            onRegisterClick  = { navController.navigate("register") },
+                            onLogin = { email, password ->
+                            }
+                        )
+                    }
+                }
             }
         }
     }
